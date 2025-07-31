8fcfb21a2d83d6a3aa07e4b84d8191b8580a38a5
2t0rHub <soyel2t0r@gmail.com>
2025-07-31 18:52:48 +0200

    Added animations for the hero section.

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -6,9 +6,11 @@ import {
   SliceComponentProps,
 } from "@prismicio/react";
 import { PrismicNextImage } from "@prismicio/next";
+
 import ButtonLink from "@/components/buttonlink";
 import Bounded from "@/components/bounded";
 import StarGrid from "@/components/stargrid";
+import AnimatedContent from "./animatedcontent";
 
 /**
  * Props for `Hero`.
@@ -25,41 +27,7 @@ const Hero: FC<HeroProps> = ({ slice }) => {
       data-slice-variation={slice.variation}
       className="text-center"
     >
-      <div className="relative">
-        <StarGrid />
-
-        {/* Heading */}
-        {isFilled.richText(slice.primary.heading) && (
-          <h1 className="text-5xl font-medium text-balance md:text-7xl">
-            <PrismicText field={slice.primary.heading} />
-          </h1>
-        )}
-
-        {/* Body */}
-        {isFilled.richText(slice.primary.body) && (
-          <div className="mx-auto mt-6 max-w-md text-balance text-slate-300">
-            <PrismicRichText field={slice.primary.body} />
-          </div>
-        )}
-
-        {/* CTA Button */}
-        {isFilled.link(slice.primary.button_link) && (
-          <ButtonLink className="mt-8" field={slice.primary.button_link}>
-            {slice.primary.button_label}
-          </ButtonLink>
-        )}
-
-        {/* Image */}
-        {isFilled.image(slice.primary.image) && (
-          <div className="glass-container mt-16 w-fit">
-            <div className="absolute inset-0 -z-10 bg-cyan-500/30 blur-2xl filter"></div>
-            <PrismicNextImage
-              className="rounded-lg"
-              field={slice.primary.image}
-            />
-          </div>
-        )}
-      </div>
+      <AnimatedContent slice={slice} />
     </Bounded>
   );
 };
